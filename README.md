f50dcb8ad96a72fb1bf395108e18f7c36a58b558
n4vxn <naveenov23@gmail.com>
2024-12-20 00:52:29 +0530

    Added: README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# FileMove
+# FileMove - TCP File Server
+
+FileMove is a simple TCP file server that allows users to upload, download, and store files securely. The server uses PostgreSQL to store user data and file metadata, ensuring smooth file transfers.
+## Features
+
+- **Upload Files**: Allows users to upload files to the server.
+- **Download Files**: Users can download files from the server.
+- **Store Metadata**: File metadata (e.g., filenames) and user information are stored in a PostgreSQL database.
+- **Simple TCP Connection**: Uses TCP for file transfer, secured with TLS.
--- cmd/main.go
+++ cmd/main.go
@@ -13,10 +13,10 @@ import (
 )
 
 var (
-	HOST     = "localhost"
-	PORT     = "8080"
-	certfile = "./tls/server.crt"
-	keyfile  = "./tls/server.key"
+	HOST          = "localhost"
+	PORT          = "8080"
+	certfile      = "./tls/server.crt"
+	keyfile       = "./tls/server.key"
 	ServerStorage = "Server-Storage"
 )
 
@@ -25,34 +25,32 @@ func main() {
 
 	var action string
 
-	for {
-		prompt := &survey.Select{
-			Message: "What would you like to do?",
-			Options: []string{"Sign Up", "Login", "Exit"},
-			Default: "Sign Up",
-		}
+	prompt := &survey.Select{
+		Message: "What would you like to do?",
+		Options: []string{"Sign Up", "Login", "Exit"},
+		Default: "Sign Up",
+	}
 
-		err := survey.AskOne(prompt, &action)
+	err := survey.AskOne(prompt, &action)
+	if err != nil {
+		log.Fatalf("Failed to get input: %v", err)
+	}
+
+	switch action {
+	case "Sign Up":
+		SignUpCmd.Run(nil, nil)
+	case "Login":
+		err := LoginCmd.RunE(nil, nil)
 		if err != nil {
-			log.Fatalf("Failed to get input: %v", err)
+			fmt.Println("Login failed, try again.")
+		} else {
+			StartServer()
 		}
-
-		switch action {
-		case "Sign Up":
-			SignUpCmd.Run(nil, nil)
-		case "Login":
-			err := LoginCmd.RunE(nil, nil)
-			if err != nil {
-				fmt.Println("Login failed, try again.")
-			} else {
-				StartServer()
-			}
-		case "Exit":
-			fmt.Println("Exiting the program...")
-			return
-		default:
-			fmt.Println("Invalid option. Please try again.")
-		}
+	case "Exit":
+		fmt.Println("Exiting the program...")
+		return
+	default:
+		fmt.Println("Invalid option. Please try again.")
 	}
 }
 
@@ -77,7 +75,7 @@ func StartServer() {
 			log.Fatalf("Server failed to start: %v", err)
 		}
 	}()
-	
+
 	CreateDir()
 	time.Sleep(2 * time.Second)
 
